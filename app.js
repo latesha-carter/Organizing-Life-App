63c736a9bc3d416837ac116240d004e731719c9b
Latesha Carter <159862436+latesha-carter@users.noreply.github.com>
2024-11-18 22:42:36 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 console.log('Organizing Life App is running');
+
+function addTask() {
+    const taskInput = document.getElementById('taskInput');
+    const taskList = document.getElementById('taskList');
+
+    if (taskInput.value !== '') {
+        const li = document.createElement('li');
+        li.textContent = taskInput.value;
+        taskList.appendChild(li);
+        taskInput.value = '';
+    }
+}
